6d785349a1777acdc40cc917ea5fbd29bc81cd74
Fadeew2007 <tarifa93@gmail.com>
2024-04-25 11:24:51 +0300

    Deploy

---
+++ Procfile
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+web: python pars_price_ukrprokat.py
+=======
+web: python pars_price_ukrprokat.py
+>>>>>>> 7078e7d (Initial commit)
---
+++ pars_price_ukrprokat.py
@@ -0,0 +1,49 @@
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+from telegram.ext import Application
+import time
+
+async def fetch_price_and_name(session, url):
+    async with session.get(url) as response:
+        content = await response.text()
+        soup = BeautifulSoup(content, 'html.parser')
+        product_name = soup.find('h1').text.strip()  # Змініть селектор, якщо потрібно
+        price_elements = soup.find_all(class_='block px-6 py-2')
+        price = price_elements[4].text.strip()  # Адаптуйте індекс, якщо структура сторінки це вимагає
+        return product_name, price
+
+async def track_price_changes(token, chat_id, url, start_delay, check_interval=30):
+    await asyncio.sleep(start_delay)  # Затримка перед початком моніторингу
+    app = Application.builder().token(token).build()
+    async with aiohttp.ClientSession() as session:
+        last_price = None
+        while True:
+            try:
+                product_name, current_price = await fetch_price_and_name(session, url)
+                if last_price is not None and current_price != last_price:
+                    message = f'Ціна на "{product_name}" змінилася з {last_price} на {current_price}'
+                    await app.bot.send_message(chat_id, text=message)
+                last_price = current_price
+                print(f'Перевірено на {time.ctime()}: {product_name} - ціна {current_price}')
+            except Exception as e:
+                print(f'Помилка під час виконання скрипта: {e}')
+            await asyncio.sleep(check_interval)
+
+async def main():
+    TOKEN = '7081365803:AAG0I20iXpdo3vDOJ_PsndeEslZzYwuUwcM'
+    CHAT_ID = '530420753'
+    products = [
+        {"url": "https://alin.ua/info-car/ford-fiesta-hatchback-lviv", "delay": 0},
+        {"url": "https://alin.ua/info-car/ravon-r2", "delay": 10},  # Затримка 10 секунд
+    ]
+
+    tasks = []
+    for product in products:
+        task = track_price_changes(TOKEN, CHAT_ID, product['url'], product['delay'])
+        tasks.append(task)
+    
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    asyncio.run(main())
